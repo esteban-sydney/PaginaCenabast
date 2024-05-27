07d08ad8b0e035a42c5ecfed0aee87046d848216
Varas Varela <stebansydney@hotmail.com>
2024-05-27 17:19:22 -0400

    Se sube proyecto

---
+++ CenabastRobotsApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+  #  path('proyectos/', views.proyectos, name='proyectos'),
+  #  path('gestioncontratos/', views.gestioncontratos, name='gestioncontratos'),
+  #  path('usuarios/', views.usuarios, name='usuarios'),
+    path('finanzas/', views.finanzas, name='finanzas'),
+    path('facturacion/', views.facturacion, name='facturacion'),
+   # path('personas/', views.personas, name='personas'),
+]
+
+
---
+++ CenabastRobotsApp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "index.html")
+
+#def proyectos(request):
+ #   return render(request, "proyectos.html")
+
+#def gestioncontratos(request):
+ #   return render(request, "gestioncontratos.html")
+
+#def usuarios(request):
+ #   return render(request, "usuarios.html")
+
+def finanzas(request):
+    return render(request, "finanzas.html")
+
+def facturacion(request):
+    return render(request, "facturacion.html")
+
+#def personas(request):
+ #   return render(request, "personas.html")
+
+
+
